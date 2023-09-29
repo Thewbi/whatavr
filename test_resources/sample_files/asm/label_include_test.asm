f8443eb022b1c20eea9759ff8a424aca5c779806
W.Bischoff <w.bischoff@geze.com>
2023-09-29 09:40:41 +0200

    Work on several compilation units a deferred address processing

--- test_resources/sample_files/asm/label_include_test.asm
+++ test_resources/sample_files/asm/label_include_test.asm
@@ -1,5 +1,6 @@
 .include "label_include.inc"
 # the second include will update the global symbol table and a warning is output
+# for every updated variable!
 .include "label_include.inc"
 
 # no error for duplicate pragma once because the second one is skipped!
@@ -10,15 +11,28 @@
 # this will cause an error since this file recursively includes itself and generates a cycle
 .include "label_include_cyclic.inc"
 
+# this overrides a variable's value in the global symbol table also the global symbol table
+# has to change the location of where this variable was declared to this new location since
+# it is overriden here
+#
+# The update/override has to cause a warning!
 .equ    OVERRIDEME = 0x0002
 
 .cseg                               ; code segments starts here ...
 .org 0x0000                         ; no interrupt vector table, just enter code
 
-    ldi r16, HIGH(RAMEND)
+# this will insert the same set of assembly instructions twice!
+# The instructions have to contained twice in the generated code otherwise the assembly has a bug!
+.include "label_include_code.inc"
+.include "label_include_code.inc"
 
 main:
+     # uses the RAMEND variable defined in "label_include.inc"
+    ldi r24, HIGH(RAMEND)
+
     clr r24
+    # teststring is used before it's address is defined!
     ldi ZH, HIGH(teststring * 2)
 
+# teststring is used before it's address is defined!
 teststring: .db "Hello world", 0
